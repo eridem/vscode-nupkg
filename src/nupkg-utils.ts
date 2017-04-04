1ae981e489215de457318a9984151e557ce85f45
eridem <m@eridem.net>
2017-04-04 18:34:56 +0200

    Fix bug with dependencies without group

--- src/nupkg-utils.ts
+++ src/nupkg-utils.ts
@@ -1,3 +1,11 @@
+function getDependenciesWithoutFramework(metadata) {
+    return metadata.dependencies && metadata.dependencies[0] && metadata.dependencies[0].dependency ? metadata.dependencies[0].dependency : [];
+}
+
+function getDependenciesByFramework(metadata) {
+    return metadata.dependencies && metadata.dependencies[0] && metadata.dependencies[0].group ? metadata.dependencies[0].group : [];
+}
+
 function createNuGetAsText(nugetSpec: any): any {
     // r is Result object
     let r = {}
@@ -5,9 +13,10 @@ function createNuGetAsText(nugetSpec: any): any {
     if (nugetSpec.package && nugetSpec.package.metadata && nugetSpec.package.metadata[0]) {
         let metadata = nugetSpec.package.metadata[0] || [];
         let metadataKeys = Object.keys(metadata).filter(m => m !== 'dependencies') || [];
-        let dependencies = metadata.dependencies && metadata.dependencies[0] && metadata.dependencies[0].group ? metadata.dependencies[0].group : [];
+        let dependenciesByFramework = getDependenciesByFramework(metadata);
+        let dependenciesWithoutFramework = getDependenciesWithoutFramework(metadata);
         let references = metadata.references && metadata.references[0] && metadata.references[0].reference ? metadata.references[0].reference : []
-        let dependencyKeys = Object.keys(dependencies) || [];
+        let dependencyByFrameworkKeys = Object.keys(dependenciesByFramework) || [];
         let frameworkAssemblies = metadata.frameworkAssemblies && metadata.frameworkAssemblies[0] && metadata.frameworkAssemblies[0].frameworkAssembly ? metadata.frameworkAssemblies[0].frameworkAssembly : [];
         let frameworkAssembliesKeys = Object.keys(frameworkAssemblies) || [];
 
@@ -20,10 +29,10 @@ function createNuGetAsText(nugetSpec: any): any {
             r['References'] = references.map(r => r.$)
         }
 
-        // Dependencies
-        if (dependencyKeys.length) r['Dependencies'] = {};
-        dependencyKeys.forEach(k => {
-            let framework = dependencies[k];
+        // Dependencies by Framework
+        if (dependencyByFrameworkKeys.length) r['Dependencies'] = r['Dependencies'] || {};
+        dependencyByFrameworkKeys.forEach(k => {
+            let framework = dependenciesByFramework[k];
             let frameworkName = framework.$ && framework.$.targetFramework ? framework.$.targetFramework : 'All Frameworks'
             r['Dependencies'][frameworkName] = {}
             let dependency = r['Dependencies'][frameworkName]
@@ -33,6 +42,12 @@ function createNuGetAsText(nugetSpec: any): any {
             } else {
                 dependency = `No dependencies.`
             }
+        })
+
+        // Dependencies without Framework
+        if (dependenciesWithoutFramework.length) r['Dependencies'] = r['Dependencies'] || {};
+        dependenciesWithoutFramework.forEach(k => {
+            r['Dependencies'][k.$.id] = k.$.version
         })
 
         // frameworkAssemblies
